32a94520069e5a0a45ccb151418e219de9e43d02
RomLmbrt <101237437+RomLmbrt@users.noreply.github.com>
2022-05-24 18:09:07 +1000

    Update bork.py

--- bork.py
+++ bork.py
@@ -5,12 +5,12 @@ def traverseBork(bork):
         for exit in exits :
             k = 0
             for elt in map :
-                if elt == exit :
+                if elt == exits[exit] :
                     k+=1
             if ( k==0 ) :
                 count +=1
         return (count != 0)
-    
+        
     def exitNotInMap(exit, map):
         count = 0
         for elt in map :
@@ -18,57 +18,31 @@ def traverseBork(bork):
                 count+=1
         return (count == 0)
 
-    def visiteOrCallback(map, location, saveGame):
+    def visiteOrCallback(map, saveGame):
+        newSaveGame = bork.save()
+        location = bork.description()
         map[location]={}
         for exit in bork.exits() :
             map[location][exit]=""
-        newSaveGame = bork.save()
+        for exit in map[location] :
+            bork.move(exit)
+            map[location][exit] = bork.description()
+            bork.restore(newSaveGame)
         #something to visit
         while ( exitsNotInMap(map[location], map ) == True ) :
-            for exit in map[location] :
-                bork.move(exit)
-                map[location][exit] = bork.description()
-                bork.restore(newSaveGame)
-                if (exitNotInMap(exit, map)):
+            for exit in map[location]:
+                if (exitNotInMap(map[location][exit], map)):
                     bork.move(exit)
-                    visiteOrCallback(map, exit, newSaveGame)
+                    map = visiteOrCallback(map, newSaveGame)
         #nothing to visit
         bork.restore(saveGame)
+        return(map)
         
    # code your solution here
     bork.restart()
-    location = bork.description()
-    saveGame = bork.save()
+    saveGame=bork.save()
     map={}
     
-    visiteOrCallback(map, location, saveGame)
+    visiteOrCallback(map, saveGame)
     
     return(map)
-            
-   # Access the bork automator like so:
-   bork.restart()
-   location = bork.description()
-   exits = bork.exits()
-   exit = "???"
-   bork.move(exit)
-   saveGame = bork.save()
-   bork.restore(saveGame)
-
-   # Access functions from the imported files like this:
-   n = graphs.N(V, E, u)
-   t = digraphs.topOrdering(V, E)
-
-   return map
-
-# The following will be run if you execute the file like python3 bork_n1234567.py
-# Your solution should not depend on this code.
-if __name__ == "__main__":
-   import borkAutomator
-   bork = borkAutomator.Bork()
-   print(traverseBork(bork))
-
-   try:
-      borkHC = borkAutomator.Bork(hardCore=True)
-      print(traverseBorkHardCore(borkHC))
-   except NameError:
-      print("Not attempting hard core mode")
