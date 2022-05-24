f0ebacf4e896af9b41f90cf569041c80c63fc9cb
RomLmbrt <101237437+RomLmbrt@users.noreply.github.com>
2022-05-24 19:35:05 +1000

    Update bork.py

--- bork.py
+++ bork.py
@@ -4,34 +4,32 @@ import digraphs
 # You can define some helper functions here if you like!
 
 def exitsNotInMap(exits, map):
-        count = 0
-        for exit in exits :
-            k = 0
-            for elt in map :
-                if elt == exits[exit] :
-                    k+=1
-            if ( k==0 ) :
-                count +=1
-        return (count != 0)
-        
-    def exitNotInMap(exit, map):
-        count = 0
+    count = 0
+    for exit in exits :
+        k = 0
         for elt in map :
-            if elt == exit :
-                count+=1
-        return (count == 0)
+            if elt == exits[exit] :
+                k+=1
+        if ( k==0 ) :
+            count +=1
+    return (count != 0)
+        
+def exitNotInMap(exit, map):
+    count = 0
+    for elt in map :
+        if elt == exit :
+            count+=1
+    return (count == 0)
 
 def traverseBork(bork):
-   # code your solution here
+    bork.restart()
+    saveGame=bork.save()
+    map={}
    
-   bork.restart()
-   saveGame=bork.save()
-   map={}
-   
-   def visiteOrCallback(map, saveGame):
+    def visiteOrCallback(map, saveGame):
         newSaveGame = bork.save()
         location = bork.description()
-        #map[location]={}
+        map[location]={}
         
         for exit in bork.exits() :
             bork.move(exit)
@@ -49,13 +47,49 @@ def traverseBork(bork):
         bork.restore(saveGame)
         return(map)
     
-   visiteOrCallback(map, saveGame)
+    visiteOrCallback(map, saveGame)
 
-   return map
+    return map
 
 # To play in hard core mode, define the function traverseBorkHardCore(bork)
 # You shoud also define traverseBork either way!
-# def traverseBorkHardCore(bork):
+
+def comeBack(memory):
+    bork.restart()
+    for exit in memory :
+        bork.move(exit)
+
+def traverseBorkHardCore(bork):
+    bork.restart()
+    map={}
+    memory=[]
+   
+    def visiteOrCallback(map, memory):
+        location = bork.description()
+        map[location]={}
+        
+        for exit in bork.exits() : 
+            bork.move(exit)
+            map[location][exit] = bork.description()
+            comeBack(memory)
+            
+        #something to visit
+        while ( exitsNotInMap(map[location], map ) == True ) :
+            for exit in map[location]:
+                if (exitNotInMap(map[location][exit], map)):
+                    bork.move(exit)
+                    memory.append(exit)
+                    visiteOrCallback(map, memory)
+                     
+        #nothing to visit
+        if memory!=[]:
+            memory.pop()
+        comeBack(memory)
+       
+    
+    visiteOrCallback(map, memory)
+
+    return map
 
 # The following will be run if you execute the file like python3 bork_n1234567.py
 # Your solution should not depend on this code.
